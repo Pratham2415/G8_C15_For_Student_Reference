5589262e9b5890e76e8ba90f0f9af6da42ce2dd4
Pratham2415 <84369631+Pratham2415@users.noreply.github.com>
2021-08-16 10:29:15 +0530

    Add files via upload

---
+++ Student_Project.py
@@ -0,0 +1,31 @@
+import turtle
+
+turtle.pensize(4)
+turtle.pencolor("OliveDrab")
+turtle.setpos(-50, 0)
+
+def repeated_tasks(c,s,a):
+    turtle.fillcolor()
+    
+    for i in [0, 1, 2]:               
+        
+        
+    
+#This function call is to make the house shelter
+
+turtle.forward(25)
+turtle.right(90)
+#This function call is to make the lower part of the house
+
+
+#The below steps till the end are for drawing a circle
+turtle.penup()
+turtle.forward(62.5)
+turtle.left(90)
+turtle.forward(25)
+turtle.right(90)
+turtle.pendown()
+
+
+
+turtle.hideturtle()
